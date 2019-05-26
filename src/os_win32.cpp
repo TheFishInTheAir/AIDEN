61485baa5ad3c666938a45b537e06d4ea5677119
Ethan Breit <afishintheair@gmail.com>
2019-05-26 14:27:04 -0400

    documented win32 abstraction.

--- src/os_win32.cpp
+++ src/os_win32.cpp
@@ -2,14 +2,17 @@
 
 #ifdef _WIN32
 
-//TODO: document this
+//This function is a wrapper over the handler function.
+//All it does is call the handler function with the proper context
 LRESULT CALLBACK WndProc(HWND win, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    //Retrieve the Window Context
 	os_win32_context *ctx = (os_win32_context*)GetWindowLongPtr(win, GWLP_USERDATA);
 
 	return ctx->win32_handler(win, msg, wParam, lParam);
 }
 
+//Win32 Class initialisation function.
 void os_win32_context::startup()
 {
 	printf("Win32 Init\n");
@@ -23,27 +26,31 @@ void os_win32_context::startup()
 	w32_class.hInstance = instance;
 	w32_class.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	w32_class.hCursor = LoadCursor(NULL, IDC_ARROW);
-	w32_class.hbrBackground = 0;//(HBRUSH)(COLOR_WINDOW+1);
+	w32_class.hbrBackground = 0;
 	w32_class.lpszMenuName = NULL;
-	w32_class.lpszClassName = "TEST";
+	w32_class.lpszClassName = "AIDEN"; //TODO: specify proper class name elsewhere.
 	w32_class.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 	printf("Win32 Init Done\n");
 
 	create_win32_window();
 }
 
+//This is the core update loop for the window.
+//It processes all events.
 void os_win32_context::window_loop_start()
 {
 	printf("Starting WIN32 Window Loop\n");
 	MSG msg;
-	
+
 	while (true) //NOTE(Ethan): This is super temorary
 	{
 		if (GetMessage(&msg, 0, 0, 0) > 0)
 		{
 			if (msg.message == WM_QUIT)
 			{
-				std::exit(1);
+                //We don't have to deal with safe exits right now so just do the
+                //fastest possible exit.
+				std::exit(1); //@UNSAFE_EXIT
 			}
 
 			TranslateMessage(&msg);
@@ -52,13 +59,16 @@ void os_win32_context::window_loop_start()
 	}
 }
 
+//Redraws the bitmap.
+//The reason this function exists is just to hide all of the internal win32 context stuff.
 void os_win32_context::window_update()
 {
-	HDC dc = GetDC(win);
+	HDC dc = GetDC(win); //Capture device context for drawing.
 	update_win32_window(dc);
 	ReleaseDC(win, dc);
 }
 
+//Generates a fun pattern, mostly used to test if the bitmap is drawing correctly.
 void os_win32_context::window_test_draw()
 {
 	int pitch = width * 4;
@@ -70,24 +80,18 @@ void os_win32_context::window_test_draw()
 		for (int x = 0; x < width; x++)
 		{
 
+            //Pattern function
             float colour = sin(x/50.f + sin(y/25.f)/5.f)*cos(y/50.f) *
                 sin(x/50.f  + sin(y/25.f)/5.f + 0.5f)*cos(y/50.f + 0.5f)*
                 sin(x/50.f  + sin(y/25.f)/5.f - 0.5f)*cos(y/50.f - 0.5f);
 
-            //colour = colour < 0.f ? 0.f : colour;
-            //*pixel = sin(x/50.f)*cos(y/50.f) * 255;
             *pixel = colour * 255;
-			//*pixel = sin(((float)x) / 120) * 255;
 			++pixel;
 
-            //*pixel = sin(x/50.f + 0.5f)*cos(y/50.f + 0.5f) * 255;
             *pixel = colour * 255;
-			//*pixel = cos(((float)x) / 10) * 100;
 			++pixel;
 
-            //*pixel = sin(x/50.f + 1.f)*cos(y/50.f + 1.f) * 255;
             *pixel = colour * 255;
-			//*pixel = cos(((float)y) / 50) * 255;
 			++pixel;
 
 			*pixel = 0;
@@ -97,6 +101,7 @@ void os_win32_context::window_test_draw()
 	}
 }
 
+//Window creation and setup function.
 void os_win32_context::create_win32_window()
 {
 	printf("Creating WIN32 Window\n"); //@LOG
@@ -111,35 +116,26 @@ void os_win32_context::create_win32_window()
 
 	win = CreateWindowEx(
 		0,
-		"TEST",
-		"TEST",
-		/* WS_OVERLAPPEDWINDOW, */
-		(WS_POPUP | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX),
-		CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080,
+		"AIDEN",   //TODO: properly specify this stuff somewhere else.
+		"AIDEN",
+		(WS_POPUP | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX), //borderless fullscreen style.
+		CW_USEDEFAULT, CW_USEDEFAULT, 1920, 1080, //TODO: variable resolution
 		NULL, NULL, instance, this);
-
-	/*win = CreateWindowEx(
-		0,//WS_BORDER,
-		APP_NAME,
-		APP_NAME,
-		/* WS_OVERLAPPEDWINDOW, *//*
-		(WS_POPUP | WS_SYSMENU | WS_MAXIMIZEBOX | WS_MINIMIZEBOX),//WS_OVERLAPPEDWINDOW | WS_THICKFRAME | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
-		CW_USEDEFAULT, CW_USEDEFAULT, 900, 600, //TODO: make this configurable
-		NULL, NULL, instance, this);*/
 
 	if (win == NULL)
 	{
-        std::cout << "Win32 Error: " << GetLastError() << std::endl;
+        std::cout << "Win32 Error: " << GetLastError() << std::endl; //@LOG
 		MessageBox(NULL, "Window Creation Failed!", "Error!",
 			MB_ICONEXCLAMATION | MB_OK);
-		std::exit(1);
+		std::exit(1); //@UNSAFE_EXIT
 	}
 
 	ShowWindow(win, cmd_show);
 	UpdateWindow(win);
-
 }
 
+//Bitmap initialisation function.
+//This is called at startup and everytime the window gets resized (which should be never in this case)
 void os_win32_context::create_bitmap()
 {
 	if (bitmap_buffer)
@@ -160,9 +156,9 @@ void os_win32_context::create_bitmap()
 	int bytes_per_pixel = 4;
 	int bitmap_memory_size = (width*height)*bytes_per_pixel;
 	bitmap_buffer = VirtualAlloc(0, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
-
 }
 
+//The function that actually draws the bitmap to the screen
 void os_win32_context::update_win32_window(HDC device_context)
 {
 
@@ -176,6 +172,9 @@ void os_win32_context::update_win32_window(HDC device_context)
 		DIB_RGB_COLORS, SRCCOPY);
 }
 
+//Processes all events from the window.
+//This is currently very big and messy but it doesn't make sense to spend that much time on
+//the win32 abstraction.
 LRESULT os_win32_context::win32_handler(HWND win, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 
@@ -184,14 +183,14 @@ LRESULT os_win32_context::win32_handler(HWND win, UINT msg, WPARAM wParam, LPARA
 	case WM_KEYDOWN:
 		switch (wParam)
 		{
-		case VK_ESCAPE:
-			std::exit(1);
+		case VK_ESCAPE: //quit immediatly when esc is pressed
+			std::exit(1); //@UNSAFE_EXIT
 			break;
 		default:
 			break;
 		}
 		break;
-	case WM_SIZE:
+	case WM_SIZE: //this should never happen besides at startup
 	{
 		RECT drawable_rect;
 		GetClientRect(win, &drawable_rect);
@@ -202,21 +201,20 @@ LRESULT os_win32_context::win32_handler(HWND win, UINT msg, WPARAM wParam, LPARA
 
 		window_test_draw();
 	} break;
-	case WM_NCCREATE:
+	case WM_NCCREATE: //Properly store the Window Context
 	{
 		SetWindowLongPtr(win, GWLP_USERDATA, (LONG_PTR)((CREATESTRUCT*)lParam)->lpCreateParams);
 		SetWindowPos(win, 0, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER); //prevents data from being cached
         return true; //Continue Window Creation
 	} break;
 	case WM_CLOSE:
-		std::exit(1);
+		std::exit(1); //@UNSAFE_EXIT
 		break;
 	case WM_DESTROY:
 		break;
-	case WM_ACTIVATEAPP:
-
+	case WM_ACTIVATEAPP: //NOTE (Ethan): I used to do something here but I don't remember lol.
 		break;
-	case WM_PAINT:
+	case WM_PAINT: //Redraw the bitmap
 	{
 		PAINTSTRUCT paint;
 		HDC device_context = BeginPaint(win, &paint);
@@ -233,7 +231,7 @@ LRESULT os_win32_context::win32_handler(HWND win, UINT msg, WPARAM wParam, LPARA
 
 	} break;
 	default:
-		return DefWindowProc(win, msg, wParam, lParam);
+		return DefWindowProc(win, msg, wParam, lParam); //default window event handling
 	}
 	return 0;
 }
@@ -258,14 +256,20 @@ uint32_t os_win32_context::get_window_height()
 
 
 //NOTE(Ethan): I just copy-pasted this from the raytracer so I'm going to need to document this later
+
+//This is a temporary struct for storing data about a thread to be created.
 typedef struct
 {
 	void* data;
 	void(*func)(void*);
 } thread_func_meta;
 
+//New thread entry point.
 DWORD WINAPI thread_func(void* data)
 {
+    //Change the thread priority to high.
+    //NOTE(Ethan): from my experience this doesn't provide any noticable difference
+    //in performance (even though its supposed to).
 	if (!SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST))
 	{
 		DWORD dwError;
@@ -279,15 +283,13 @@ DWORD WINAPI thread_func(void* data)
 	return 0;
 }
 
+//Abstraction over the win32 thread creation code, fairly self explanitory.
 void os_win32_context::start_thread(void(*func)(void*), void* data)
 {
-	thread_func_meta* meta = (thread_func_meta*)malloc(sizeof(thread_func_meta));
+	thread_func_meta* meta = new thread_func_meta();
 	meta->data = data;
 	meta->func = func;
 	HANDLE t = CreateThread(NULL, 0, thread_func, meta, 0, NULL);
-	//if(SetThreadPriority(t, THREAD_PRIORITY_HIGHEST)==0)
-	//    assert(false);
-
 }
 
 #endif
